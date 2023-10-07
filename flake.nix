bdd9f49d91d156a2d91d5b29a3343fc74307d80b
Rene Schallner <rene@renerocks.ai>
2023-10-07 18:17:02 +0200

    removed docker
    the image would get huge. i suppose because of xdg-utils

--- flake.nix
+++ flake.nix
@@ -22,10 +22,10 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system; };
-        appEnv = with pkgs (ps: [
+        appEnv = with pkgs; [
             pandoc
             xdg-utils
-        ]);
+        ];
       in rec {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
@@ -53,61 +53,36 @@
         packages.mdmail = pkgs.stdenv.mkDerivation rec {
           name = "mdmail";
           buildInputs = with pkgs; [
-            bashInteractive 
+            appEnv
+            coreutils
           ];
 
-          script = pkgs.writeShellScript "mdmail" ''
+          builder = pkgs.writeShellScript "builder" ''
+            #!/bin/sh
+            export PATH="${pkgs.coreutils}/bin:$PATH"
+            mkdir -p $out/bin
+            cp $src/mail.template $out/
+            cp $src/mail.css $out/
+
+            cat > $out/bin/mdmail <<EOF
             #!/usr/bin/env bash
-            if [ -z "$1" ] ; then
+            if [ -z "\$1" ] ; then
                 echo "no arg"
                 exit
             fi
 
-            md="$1"
-            html="/tmp/$1.html"
-            ${appEnv}/bin/pandoc --embed-resources --css=${out}/mail.css --template=${out}/mail.template -f markdown -t html $md -o $html
-            ${appEnv}/bin/xdg-open 
+            md="\$1"
+            html="/tmp/\$1.html"
+            ${pkgs.pandoc.out}/bin/pandoc --embed-resources --css=$out/mail.css --template=$out/mail.template -f markdown -t html \$md -o \$html
+            ${pkgs.xdg-utils.out}/bin/xdg-open \$html
+            EOF
+            chmod +x $out/bin/mdmail
           '';
 
           src = ./.;
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ./mail.template $out/
-            cp ./mail.css $out/
-            cp ${script} $out/bin/mdmail
-          '';
-
         };
         defaultPackage = self.packages.${system}.mdmail;
-
-        # Usage:
-        #    nix build .#docker
-        #    docker load < result
-        #    docker run -p5000:5000 vianda:lastest
-        packages.docker = pkgs.dockerTools.buildImage {
-          name = "mdmail";       # give docker image a name
-          tag = "latest";     # provide a tag
-          created = "now";
-
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            # paths = [ packages.mdmail appEnv pkgs.coreutils];
-            paths = [ packages.mdmail appEnv ];
-            pathsToLink = [ "/bin" "/tmp" ];
-          };
-
-          config = {
-            Cmd = [ "${packages.mdmail}/bin/mdmail" ];
-            WorkingDir = "/bin";
-            Volumes = { 
-                "/tmp" = { }; 
-                };
-            ExposedPorts = {
-            };
-          };
-        };
-
 
     });
 }
